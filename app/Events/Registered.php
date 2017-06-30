7bc4cb50c65cd95a3ece0b09a29faceb5ce1190c
Janssen <larsjanssen64@gmail.com>
2017-06-30 23:34:52 +0200

    Create new registered event

---
+++ app/Events/Registered.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use App\Dealcloser\Core\User\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class Registered
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var User
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+}
