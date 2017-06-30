ebad464cc9402ae61693c92672e2c5def682391a
Janssen <larsjanssen64@gmail.com>
2017-06-30 23:30:45 +0200

    Implement UserRepo

---
+++ app/Dealcloser/Interfaces/Repositories/IUserRepo.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Dealcloser\Interfaces\Repositories;
+
+interface IUserRepo
+{
+    public function store(array $user);
+}
---
+++ app/Dealcloser/Repositories/User/UserRepo.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Dealcloser\Repositories\User;
+
+use App\Dealcloser\Core\User\User;
+use App\Dealcloser\Interfaces\Repositories\IUserRepo;
+
+class UserRepo implements IUserRepo
+{
+    /**
+     * Store a user
+     *
+     * @param array $user
+     * @return User
+     */
+    public function store(array $user)
+    {
+        return User::create($user);
+    }
+
+    /**
+     * Check if value exists
+     *
+     * @param $column
+     * @param $value
+     * @return mixed
+     */
+    public function exists($column, $value)
+    {
+        return User::where($column, $value)->exists();
+    }
+
+    /**
+     * Activate user
+     *
+     * @param $column
+     * @param $token
+     * @return User $user
+     */
+    public function activate($column, $token)
+    {
+        return tap(User::where($column, $token)->first(), function ($user) use ($column) {
+            $user->update(['active' => true, $column => null]);
+        });
+    }
+}
+
