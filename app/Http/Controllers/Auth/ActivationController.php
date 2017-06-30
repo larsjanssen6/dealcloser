5194f804db648b4bfed88c119ec9d10e7f0f33fa
Janssen <larsjanssen64@gmail.com>
2017-06-30 23:35:24 +0200

    A user can be activated

---
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Dealcloser\Interfaces\Repositories\IUserRepo;
+use Auth;
+use App\Http\Controllers\Controller;
+
+class ActivationController extends Controller
+{
+    /**
+     * IUserRepo implementation
+     *
+     * @var IUserRepo
+     */
+    private $userRepo;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param IUserRepo $userRepo
+     */
+    public function __construct(IUserRepo $userRepo)
+    {
+        $this->userRepo = $userRepo;
+    }
+
+    /**
+     * Show user activation form
+     *
+     * @param $token
+     * @return \Illuminate\View\View|\Illuminate\Http\RedirectResponse
+     */
+    public function show($token)
+    {
+        if($this->exists($token)) {
+            return view('auth.activate')->with([
+                'token' => $token
+            ]);
+        }
+
+        return $this->unAuthorized();
+    }
+
+    /**
+     * Activate user
+     *
+     * @param $token
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function activate($token)
+    {
+        if($this->exists($token)) {
+            Auth::login($this->userRepo->activate('confirmation_code', $token));
+
+            return redirect('/dashboard')
+                ->with('status', sprintf('Welkom uw account is geactiveerd'));
+        }
+
+        return $this->unAuthorized();
+    }
+
+    /**
+     * Check if token exists
+     *
+     * @param $token
+     * @return bool
+     */
+    public function exists($token)
+    {
+        return $this->userRepo->exists('confirmation_code', $token);
+    }
+
+    /**
+     * Redirect with unauthorized message
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function unAuthorized()
+    {
+        return redirect('/')
+            ->with([
+                'status' => 'Niet geautoriseerd!',
+                'class' => 'is-danger'
+            ]);
+    }
+}
