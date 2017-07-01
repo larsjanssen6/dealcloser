ed1f90803c6be2c55560e25759d60aa16803b134
Janssen <larsjanssen64@gmail.com>
2017-07-01 12:38:44 +0200

    Max from 20 to 50

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
             'name'      => 'min:3|max:50|required',
             'last_name' => 'min:3|max:50|required',
             'email'     => 'min:5|max:50|email|unique:users',
-            'function'  => 'min:3|max:20|nullable',
+            'function'  => 'min:3|max:50|nullable',
             'role'      => 'required|exists:roles,name'
         ];
     }
