37dfbda5af4b5e2365e0d9243b2b71b37eadc982
Janssen <larsjanssen64@gmail.com>
2017-07-18 12:40:52 +0200

    Rename relation to organisation

--- resources/assets/js/components/organisation/Organisation.vue
+++ resources/assets/js/components/organisation/Organisation.vue
@@ -71,7 +71,7 @@
                     <p><a :href="prpOrganisation.website">{{ prpOrganisation.website }}</a></p>
                 </div>
 
-                <div class="column" v-if="prpRelation.products.length != 0">
+                <div class="column" v-if="prpOrganisation.products.length != 0">
                     <strong>Producten</strong>
                     <p>
                          <span class="tag is-warning is-medium" v-for="product in prpOrganisation.products">
