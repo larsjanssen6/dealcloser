7131212be93ad215bfb5f482abd4770d26c5e640
Janssen <larsjanssen64@gmail.com>
2017-07-31 14:05:22 +0200

    Load with json rename pro

--- resources/assets/js/components/user/UpdateUser.vue
+++ resources/assets/js/components/user/UpdateUser.vue
@@ -2,222 +2,215 @@
     <div v-if="show">
         <modal-card @close="show = false">
             <div slot="title">
-                <p>Bewerk {{ user.name }} {{ user.preposition }} {{ user.last_name }}</p>
+                <p v-if="loadingForm">.....</p>
+                <p v-else>Bewerk {{ fullName }}</p>
             </div>
 
             <slot>
                 <form>
-                    <div class="field">
-                        <label for="name" class="label">Naam</label>
+                    <div v-if="loadingForm">
+                        <p>Gebruiker aan het inladen...</p>
+                    </div>
 
-                        <div class="control">
-                            <input id="name"
-                                   name="name"
-                                   type="text"
-                                   v-model="user.name"
-                                   class="input"
-                                   :class="{ 'is-danger': errorsHas('name') }"
-                                   autofocus>
+                    <div v-else>
+                        <custom-input
+                                :value.sync="user.name"
+                                label="Naam"
+                                name="name"
+                                type="text">
+                        </custom-input>
 
-                            <p class="help is-danger" v-if="errorsHas('name')">{{ error('name') }}</p>
+                        <custom-input
+                                :value.sync="user.preposition"
+                                label="Tussenvoegsel"
+                                name="preposition"
+                                type="text">
+                        </custom-input>
+
+                        <custom-input
+                                :value.sync="user.last_name"
+                                label="Achternaam"
+                                name="last_name"
+                                type="text">
+                        </custom-input>
+
+                        <custom-input
+                                :value.sync="user.email"
+                                label="Email"
+                                name="email"
+                                type="text">
+                        </custom-input>
+
+                        <div class="field">
+                            <label for="password" class="label">Wachtwoord</label>
+
+                            <div class="control">
+                                <input id="password"
+                                       name="password"
+                                       type="password"
+                                       placeholder="**********"
+                                       v-model="user.password"
+                                       class="input"
+                                       :class="{ 'is-danger': errorsHas('password') }">
+
+                                <p class="help is-danger" v-if="errorsHas('password')">{{ error('password') }}</p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="field">
-                        <label for="preposition" class="label">Tussenvoegsel</label>
+                        <div class="field">
+                            <label for="password_confirmation" class="label">Bevestig wachtwoord</label>
 
-                        <div class="control">
-                            <input id="preposition"
-                                   name="preposition"
-                                   type="text"
-                                   v-model="user.preposition"
-                                   class="input"
-                                   :class="{ 'is-danger': errorsHas('preposition') }"
-                                   autofocus>
+                            <div class="control">
+                                <input id="password_confirmation"
+                                       name="password_confirmation"
+                                       type="password"
+                                       placeholder="**********"
+                                       v-model="user.password_confirmation"
+                                       class="input"
+                                       :class="{ 'is-danger': errorsHas('password_confirmation') }">
 
-                            <p class="help is-danger" v-if="errorsHas('preposition')">{{ error('preposition') }}</p>
+                                <p class="help is-danger" v-if="errorsHas('password_confirmation')">{{
+                                    error('password_confirmation') }}</p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="field">
-                        <label for="last_name" class="label">Achternaam</label>
+                        <custom-input
+                                :value.sync="user.function"
+                                label="Functie"
+                                name="function"
+                                type="text">
+                        </custom-input>
 
-                        <div class="control">
-                            <input id="last_name"
-                                   name="last_name"
-                                   type="text"
-                                   v-model="user.last_name"
-                                   class="input"
-                                   :class="{ 'is-danger': errorsHas('last_name') }">
+                        <div class="field">
+                            <label for="department_id" class="label">Afdeling</label>
 
-                            <p class="help is-danger" v-if="errorsHas('last_name')">{{ error('last_name') }}</p>
+                            <select id="department_id" name="department_id"
+                                    class="input" :class="{ 'is-danger': errorsHas('department_id') }"
+                                    v-model="user.department_id">
+                                <option disabled value="">Selecteer een afdeling</option>
+
+                                <option v-for="department in departments" :value="department.id">
+                                    {{ department.name }}
+                                </option>
+                            </select>
                         </div>
-                    </div>
 
-                    <div class="field">
-                        <label for="email" class="label">Email</label>
+                        <div class="field">
+                            <label for="role" class="label">Rol</label>
 
-                        <div class="control">
-                            <input id="email"
-                                   name="email"
-                                   type="text"
-                                   v-model="user.email"
-                                   class="input"
-                                   :class="{ 'is-danger': errorsHas('email') }">
+                            <select id="role" name="role"
+                                    class="input" :class="{ 'is-danger': errorsHas('role') }" v-model="user.role">
+                                <option disabled value="">Selecteer een rol</option>
 
-                            <p class="help is-danger" v-if="errorsHas('email')">{{ error('email') }}</p>
+                                <option v-for="role in roles" :value="role.name">
+                                    {{ role.name }}
+                                </option>
+                            </select>
                         </div>
-                    </div>
 
-                    <div class="field">
-                        <label for="password" class="label">Wachtwoord</label>
+                        <div class="field">
+                            <label for="active" class="label">Actief</label>
 
-                        <div class="control">
-                            <input id="password"
-                                   name="password"
-                                   type="password"
-                                   placeholder="**********"
-                                   v-model="user.password"
-                                   class="input"
-                                   :class="{ 'is-danger': errorsHas('password') }">
+                            <select id="active"
+                                    name="active"
+                                    class="input"
+                                    :class="{ 'is-danger': errorsHas('active') }"
+                                    v-model="user.active">
 
-                            <p class="help is-danger" v-if="errorsHas('password')">{{ error('password') }}</p>
+                                <option selected disabled>Selecteer Ja of Nee</option>
+
+                                <option value="1">Ja</option>
+                                <option value="0">Nee</option>
+                            </select>
                         </div>
-                    </div>
-
-                    <div class="field">
-                        <label for="password_confirmation" class="label">Bevestig wachtwoord</label>
-
-                        <div class="control">
-                            <input id="password_confirmation"
-                                   name="password_confirmation"
-                                   type="password"
-                                   placeholder="**********"
-                                   v-model="user.password_confirmation"
-                                   class="input"
-                                   :class="{ 'is-danger': errorsHas('password_confirmation') }">
-
-                            <p class="help is-danger" v-if="errorsHas('password_confirmation')">{{
-                                error('password_confirmation') }}</p>
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <label for="function" class="label">Functie</label>
-
-                        <div class="control">
-                            <input id="function"
-                                   name="function"
-                                   type="text"
-                                   v-model="user.function"
-                                   class="input"
-                                   :class="{ 'is-danger': errorsHas('function') }">
-
-                            <p class="help is-danger" v-if="errorsHas('function')">{{ error('function') }}</p>
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <label for="department_id" class="label">Afdeling</label>
-
-                        <select id="department_id" name="department_id"
-                                class="input" :class="{ 'is-danger': errorsHas('department_id') }"
-                                v-model="user.department_id">
-                            <option disabled value="">Selecteer een afdeling</option>
-
-                            <option v-for="department in prpDepartments" :value="department.id">
-                                {{ department.name }}
-                            </option>
-                        </select>
-                    </div>
-
-                    <div class="field">
-                        <label for="role" class="label">Rol</label>
-
-                        <select id="role" name="role"
-                                class="input" :class="{ 'is-danger': errorsHas('role') }" v-model="user.role">
-                            <option disabled value="">Selecteer een rol</option>
-
-                            <option v-for="role in prpRoles" :value="role.name">
-                                {{ role.name }}
-                            </option>
-                        </select>
-                    </div>
-
-                    <div class="field">
-                        <label for="active" class="label">Actief</label>
-
-                        <select id="active"
-                                name="active"
-                                class="input"
-                                :class="{ 'is-danger': errorsHas('active') }"
-                                v-model="user.active">
-
-                            <option selected disabled>Selecteer Ja of Nee</option>
-
-                            <option value="1">Ja</option>
-                            <option value="0">Nee</option>
-                        </select>
                     </div>
                 </form>
             </slot>
 
             <div slot="footer">
-                <button id="submit"
-                        class="button is-primary"
-                        :class="{ 'is-loading': loading }"
-                        @click="update">
-                    Update
-                </button>
+                <div v-if="loadingForm">
+                    <a class="button is-primary is-outlined"
+                       @click="show = false">
+                        Annuleer
+                    </a>
+                </div>
 
-                <destroy :service="UserService" :id="user.id"></destroy>
+                <div v-else>
+                    <button id="submit"
+                            class="button is-primary"
+                            :class="{ 'is-loading': loading }"
+                            @click="update">
+                        Update
+                    </button>
 
-                <a class="button is-primary is-outlined"
-                   @click="show = false">
-                    Annuleer
-                </a>
+                    <destroy :service="UserService" :id="user.id"></destroy>
+
+                    <a class="button is-primary is-outlined"
+                       @click="show = false">
+                        Annuleer
+                    </a>
+                </div>
             </div>
         </modal-card>
     </div>
 </template>
 
 <script>
-    import UserService from '../../services/UserService';
     import Validation from '../../mixins/validation.js';
+    import RoleService from "../../services/RoleService";
+    import UserService from '../../services/UserService';
+    import ProductService from '../../services/ProductService';
     import DepartmentService from '../../services/DepartmentService';
 
     export default {
-        props: ['prp-user', 'prp-departments', 'prp-roles', 'prp-products'],
+        props: ['user-id'],
 
         mixins: [Validation],
 
         data() {
             return {
+                loadingForm: false,
                 loading: false,
                 show: false,
+                departments: [],
+                roles: [],
+                products: [],
                 user: { role: "" },
                 UserService: UserService
             }
         },
 
         created() {
-            this.user = this.prpUser;
 
-            /*
-             | Set the first role on the user object. Maybe in the future
-             | a user can have multiple roles.
+            /**
+             * Listen for show user modal event.
              */
 
-            this.user.role = this.prpUser.roles[0].name;
-
             Event.$on('show-user-modal', (id) => {
-                if (this.user.id === id) {
+                if (this.userId === id) {
+                    this.getUser(id);
                     this.show = true;
                 }
             });
         },
 
+        computed: {
+
+            /**
+             * Get full name.
+             */
+
+            fullName: {
+                get() { return this.user.name + ' ' + (this.user.preposition === null ? '' : this.user.preposition)  + ' ' + this.user.last_name }
+            }
+        },
+
         methods: {
+
+            /**
+             * Update the user.
+             */
+
             update() {
                 this.loading = true;
 
@@ -238,6 +231,46 @@
                         this.loading = false;
                         this.errors = error.response.data;
                     });
+            },
+
+            /**
+             * Get user.
+             */
+
+            getUser(id) {
+                this.loadingForm = true;
+
+                UserService.show(id).then(({data}) => {
+                    this.user = data;
+
+                    this.getDepartments();
+                    this.getRoles();
+
+                    /**
+                     * Take first role of roles. Maybe in the future
+                     * a user can have multiple roles.
+                     */
+
+                    this.user.role = this.user.roles[0].name;
+
+                    this.loadingForm = false;
+                });
+            },
+
+            /**
+             * Get all departments.
+             */
+
+            getDepartments() {
+                DepartmentService.index().then(({data}) => this.departments = data);
+            },
+
+            /**
+             * Get all roles.
+             */
+
+            getRoles() {
+                RoleService.index().then(({data}) => this.roles = data);
             }
         }
     }
--- resources/assets/js/components/user/User.vue
+++ resources/assets/js/components/user/User.vue
@@ -2,57 +2,64 @@
     <div v-if="show">
         <modal-card @close="show = false">
             <div slot="title">
-                <p>{{ prpUser.fullName }}</p>
+                <p v-if="loadingForm">.....</p>
+                <p v-else>{{ user.fullName }}</p>
             </div>
 
             <slot>
-                <div class="column">
-                    <strong>Voornaam</strong>
-                    <p>{{ prpUser.name }}</p>
+                <div v-if="loadingForm">
+                    <p>Laden...</p>
                 </div>
 
-                <div class="column" v-if="prpUser.preposition">
-                    <strong>Tussenvoegsel</strong>
-                    <p>{{ prpUser.preposition }}</p>
-                </div>
+                <div v-else>
+                    <div class="column">
+                        <strong>Voornaam</strong>
+                        <p>{{ user.name }}</p>
+                    </div>
 
-                <div class="column">
-                    <strong>Achternaam</strong>
-                    <p>{{ prpUser.last_name }}</p>
-                </div>
+                    <div class="column" v-if="user.preposition">
+                        <strong>Tussenvoegsel</strong>
+                        <p>{{ user.preposition }}</p>
+                    </div>
 
-                <div class="column">
-                    <strong>Functie</strong>
-                    <p>{{ prpUser.function }}</p>
-                </div>
+                    <div class="column">
+                        <strong>Achternaam</strong>
+                        <p>{{ user.last_name }}</p>
+                    </div>
 
-                <div class="column">
-                    <strong>Email</strong>
-                    <p><a :href="'mailto:' + prpUser.email">{{ prpUser.email }}</a></p>
-                </div>
+                    <div class="column">
+                        <strong>Functie</strong>
+                        <p>{{ user.function }}</p>
+                    </div>
 
-                <div class="column">
-                    <strong>Afdeling</strong>
-                    <p>
+                    <div class="column">
+                        <strong>Email</strong>
+                        <p><a :href="'mailto:' + user.email">{{ user.email }}</a></p>
+                    </div>
+
+                    <div class="column">
+                        <strong>Afdeling</strong>
+                        <p>
                         <span class="tag is-success">
-                            {{ prpUser.department.name }}
+                            {{ user.department.name }}
                         </span>
-                    </p>
-                </div>
+                        </p>
+                    </div>
 
-                <div class="column">
-                    <strong>Role</strong>
+                    <div class="column">
+                        <strong>Role</strong>
 
-                    <p>
-                        <span class="tag is-success" v-for="role in prpUser.roles">
+                        <p>
+                        <span class="tag is-success" v-for="role in user.roles">
                             {{ role.name }}
                         </span>
-                    </p>
+                        </p>
+                    </div>
                 </div>
             </slot>
 
             <div slot="footer">
-                <a :href="'mailto:' + prpUser.email" class="button is-primary">Contact</a>
+                <a :href="'mailto:' + user.email" class="button is-primary">Contact</a>
                 <a class="button is-primary is-outlined" @click="show = false">Terug</a>
             </div>
         </modal-card>
@@ -60,21 +67,41 @@
 </template>
 
 <script>
+    import UserService from "../../services/UserService";
+
     export default {
-        props: ['prp-user'],
+        props: ['user-id'],
 
         data() {
             return {
                 show: false,
+                user: {}
             }
         },
 
         created() {
             Event.$on('show-user-modal', (id) => {
-                if (this.prpUser.id === id) {
+                if (this.userId === id) {
+                    this.getUser(id);
                     this.show = true;
                 }
             });
+        },
+
+        methods: {
+
+            /**
+             * Get user.
+             */
+
+            getUser(id) {
+                this.loadingForm = true;
+
+                UserService.show(id).then(({data}) => {
+                    this.user = data;
+                    this.loadingForm = false;
+                });
+            },
         }
     }
 </script>
